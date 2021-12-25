cd46a0e43cea805414b8f476dbbd4c2444263f42
SuperMiky <chapo2303@gmail.com>
2021-12-26 00:26:34 +0100

    Costruzione del pacchetto che arriva al server pt.1 (lavorazione sulle classi Pacchetto e DatiCondivisi)

--- TetrisGame/TetrisGame/DatiCondivisi.cs
+++ TetrisGame/TetrisGame/DatiCondivisi.cs
@@ -8,5 +8,23 @@ namespace TetrisGame
 {
     class DatiCondivisi
     {
+        private List<Pacchetto> DaElaborare;
+
+        public void addDaElaborare(Pacchetto p) //aggiungo il pacchetto da elaborare
+        {
+            DaElaborare.Add(p);
+        }
+
+        public Pacchetto getDaElaborare() //prendo il pacchetto da elaborare
+        {
+            Pacchetto p = DaElaborare[DaElaborare.Count - 1]; //prendo l'ultimo pacchetto
+            DaElaborare.RemoveAt(DaElaborare.Count - 1); //lo rimuovo dalla lista
+            return p;
+        }
+
+        public int getSizeDaElaborare() //mi restituisce il n degli elementi della lista
+        {
+            return DaElaborare.Count;
+        }
     }
 }
--- TetrisGame/TetrisGame/Pacchetto.cs
+++ TetrisGame/TetrisGame/Pacchetto.cs
@@ -8,5 +8,24 @@ namespace TetrisGame
 {
     class Pacchetto
     {
+        private int stato, righe, malus; //stato del gioco, righe risolte, malus
+        private String nome; //nome del giocatore
+        private char connessione; //stato connessione
+        
+        public Pacchetto(String csv) //arriva un pacchetto, lo devo splittare
+        {
+            String[] dati = csv.Split(';'); //splitto i dati
+            if(dati[0] == "g") //se si tratta di un pacchetto game
+            {
+                stato = int.Parse(dati[1]); //salvo lo stato del gioco
+                righe = int.Parse(dati[2]); //salvo le righe
+                malus = int.Parse(dati[3]); //salvo il malus 
+            }
+            else
+            {
+                connessione = char.Parse(dati[0]); //stato connessione
+                nome = dati[1]; //nome 
+            }
+        }
     }
 }
