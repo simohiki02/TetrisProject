af9c11aaecbc1c204a71f021d6c0acb6ae4c6574
Simone-vazzoler <vazzolersimone0@gmail.com>
2021-12-31 17:28:48 +0100

    Inizio creazione classe blocco, con metodo reset per le posizioni dei blocchi e metodi per restituire le posizioni (da definire)

--- TetrisGame/TetrisGame/Blocco.cs
+++ TetrisGame/TetrisGame/Blocco.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+
 
 namespace TetrisGame
 {
     class Blocco
     {
+        //array contenente i pezzi
+        string[] arrayTetrominos = { "", "pezzoQ", "pezzoI", "pezzoT", "pezzoS", "pezzoZ", "pezzoJ", "pezzoL" };
+
+        //array contenente le rotazioni dei pezzi
+        int[] posizioni;
+
+        //array contenente i colori dei pezzi
+        //Color[] shapeColor = { coloreQ,coloreI,coloreT,coloreS,coloreZ,coloreJ,coloreL};
+
+        //id per distinguere i vari blocchi
+        public int id;
+
+        //valori per determinare posizioni dei pezzi
+        private int statoRotazione;
+        private int posizionePartenza;
+        private int posizioneCorrente;
+
+        //costruttore da definire classe per posizione 
+        public Blocco()
+        {
+            posizioneCorrente = posizionePartenza;
+        }
+
+        //metodo per resettare posizioni dei blocchi
+        public void reset()
+        {
+            statoRotazione = 0;
+            posizionePartenza = 0;
+
+        }
+
+        //metodo per rotazione dei pezzi
+
+
+        //metodo per muovere un pezzo di un numero di righe e colonne
     }
 }
--- TetrisGame/TetrisGame/GraficaTetris/Q.cs
+++ TetrisGame/TetrisGame/GraficaTetris/Q.cs
@@ -7,7 +7,7 @@ using System.Windows.Media;
 
 namespace TetrisGame.GraficaTetris
 {
-    class Q
+    public class Q
     {
         private static Color coloreQ = Colors.Yellow;
 
