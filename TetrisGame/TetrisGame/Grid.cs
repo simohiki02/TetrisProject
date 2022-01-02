0b39328cb4c107568a5b224ae752e62667638f23
Simone-vazzoler <vazzolersimone0@gmail.com>
2022-01-02 17:52:06 +0100

    -Continuo classe Grid con metodi per verificare se una riga data è occupata o no, -se è all'interno della Grid o no, quindi se è valida -inizio metodi per spostare riga di tot posti

--- TetrisGame/TetrisGame/Grid.cs
+++ TetrisGame/TetrisGame/Grid.cs
@@ -9,7 +9,7 @@ namespace TetrisGame
     public class Grid
     {
         //classe che imposta il campo di gioco
-        private int[,] campo;
+        private int[,] campo { get; set; }
 
         public int righe;
         public int colonne;
@@ -28,6 +28,73 @@ namespace TetrisGame
         {
             this.righe = righe;
             this.colonne = colonne;
+            campo = new int[righe, colonne];
+        }
+
+        public bool isInside(int r, int c)
+        {
+            bool valida = false;
+            if(r >= 0 && r < righe && c >= 0 && c < colonne)
+            {
+                valida = true;
+                return valida;
+            }
+            else
+            {
+                return valida;
+            }
+
+        }
+
+        public bool isEmpty(int r, int c)
+        {
+            bool vuota = false;
+            if(isInside(r,c) && campo[r,c] == 0)
+            {
+                vuota = true;
+                return vuota;
+            }
+            else
+            {
+                return vuota;
+            }
+        }
+
+        public bool rigaPiena(int r)
+        {
+            for(int c = 0; c < colonne; c++)
+            {
+                if(campo[r, c] == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool rigaVuota(int r)
+        {
+            for (int c = 0; c < colonne; c++)
+            {
+                if (campo[r, c] != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void pulisciRiga(int r)
+        {
+            for (int c = 0; c < colonne; c++)
+            {
+                campo[r, c] = 0;
+            }
+        }
+        //metodo per spostare di tot righe sotto una riga
+        public void muoviRiga(int r, int numeroRighe)
+        {
+
         }
     }
 }
